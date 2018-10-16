1ad6ad1cb26a0d26ae00c4c0828d2846f5087548
LovecraftianHorror <LovecraftianHorror@pm.me>
2018-10-16 12:30:27 -0400

    Reduce nesting

--- scc.py
+++ scc.py
@@ -29,59 +29,59 @@ def sep():
 if arg.rand:
 	with open(f"{home}/.scc/ccs/en/{arg.rand}.json") as file:
 		data = json.load(file)
-		key = random.choice(list(data))
-		sep()
-		print(key)
-		sep()
-		for k, v in data[key].items():
-			if k == "Attribute(s)":
-				break
-			print(f"\033[4;33m{k}\033[0m :{v}")
+	key = random.choice(list(data))
+	sep()
+	print(key)
+	sep()
+	for k, v in data[key].items():
+		if k == "Attribute(s)":
+			break
+		print(f"\033[4;33m{k}\033[0m :{v}")
 
 
 # html part
 if not arg.html:
 	with open(f"{home}/.scc/ccs/en/html.json") as file:
 		data = json.load(file)
-		if arg.export == "html":
-			print("exporting to html ...")
-			html_output = '<html><head><title>SCC-HTML CHEAT CHEET</title></head><body><style>body{ max-width:100vw ; font-family:"roboto","open sans" , "ubuntu" ,"sans-serif";background-color:#fff;color:#666; }#tag{font-size:1.2em ; color:#006688 ; background-color:#ddd ;display:inline-block ;  border-radius:10px ; padding:0.7em ;  } .xmp_tags{ padding:10px ;border-radius:5px ;word-wrap: break-word;font-family:monospace } .xmp_tags:first-child()::before{ content: "Tag : " ;}#wrapper{background-color:#eee;padding:1em ;border-radius:10px ; margin:0.5em auto ; max-width:95vw ;word-wrap: break-word; overflow:auto ; }h1{text-align:center; }</style><h1>SCC - HTML Cheat Cheet</h1>'
-			key_elem = ""
-			for tag in data:
-				key_elem += f"<div id='wrapper'><span id='tag'>{tag}</span>"
-				for desc in data[tag]:
-					if desc == list(data[tag])[0]:
-						continue
-					if desc == "Attribute(s)":
-						key_elem += "--- Attributes ---"
-						attrs = data[tag][desc]
-						for attr in attrs:
-							key_elem += f"<xmp class='xmp_tags'>{attr}:{data[tag][desc][attr]}</xmp>"
-						continue
-					key_elem += f"<xmp class='xmp_tags'>{data[tag][desc]}</xmp>"
-				key_elem += "</div>"
-			html_output += key_elem
+	if arg.export == "html":
+		print("exporting to html ...")
+		html_output = '<html><head><title>SCC-HTML CHEAT CHEET</title></head><body><style>body{ max-width:100vw ; font-family:"roboto","open sans" , "ubuntu" ,"sans-serif";background-color:#fff;color:#666; }#tag{font-size:1.2em ; color:#006688 ; background-color:#ddd ;display:inline-block ;  border-radius:10px ; padding:0.7em ;  } .xmp_tags{ padding:10px ;border-radius:5px ;word-wrap: break-word;font-family:monospace } .xmp_tags:first-child()::before{ content: "Tag : " ;}#wrapper{background-color:#eee;padding:1em ;border-radius:10px ; margin:0.5em auto ; max-width:95vw ;word-wrap: break-word; overflow:auto ; }h1{text-align:center; }</style><h1>SCC - HTML Cheat Cheet</h1>'
+		key_elem = ""
+		for tag in data:
+			key_elem += f"<div id='wrapper'><span id='tag'>{tag}</span>"
+			for desc in data[tag]:
+				if desc == list(data[tag])[0]:
+					continue
+				if desc == "Attribute(s)":
+					key_elem += "--- Attributes ---"
+					attrs = data[tag][desc]
+					for attr in attrs:
+						key_elem += f"<xmp class='xmp_tags'>{attr}:{data[tag][desc][attr]}</xmp>"
+					continue
+				key_elem += f"<xmp class='xmp_tags'>{data[tag][desc]}</xmp>"
+			key_elem += "</div>"
+		html_output += key_elem
 
-			with open("exported-scc.html", "w") as file:
-				file.write(html_output)
-			webbrowser.open_new_tab("exported-scc.html")
-			print("Done.")
-			exit()
-		print("\n".join(data))
+		with open("exported-scc.html", "w") as file:
+			file.write(html_output)
+		webbrowser.open_new_tab("exported-scc.html")
+		print("Done.")
+		exit()
+	print("\n".join(data))
 elif arg.html != "scc_empty":
 	try:
 		with open(f"{home}/.scc/ccs/en/html.json") as file:
 			data = json.load(file)
-			for key, val in data[arg.html.rstrip()].items():
-				if key == "Attribute(s)":
-					sep()
-					print("--- Attributes ---".rjust(40))
-					sep()
-					for elem, info in val.items():
-						print(f"\033[4;33m• {elem}\u001b[0m : {info}")
-					sep()
-					exit()
-				print(f"\033[4;33m{key}\u001b[0m : {val}")
+		for key, val in data[arg.html.rstrip()].items():
+			if key == "Attribute(s)":
+				sep()
+				print("--- Attributes ---".rjust(40))
+				sep()
+				for elem, info in val.items():
+					print(f"\033[4;33m• {elem}\u001b[0m : {info}")
+				sep()
+				exit()
+			print(f"\033[4;33m{key}\u001b[0m : {val}")
 	except KeyError:
 		print("This HTML tag doesn't exist")
 
@@ -89,13 +89,13 @@ elif arg.html != "scc_empty":
 if not arg.css:
 	with open(f"{home}/.scc/ccs/en/css.json") as file:
 		data = json.load(file)
-		print("\n".join(data))
+	print("\n".join(data))
 elif arg.css != "scc_empty":
 	try:
 		with open(f"{home}/.scc/ccs/en/css.json") as file:
 			data = json.load(file)
-			for key, val in data[arg.css.rstrip()].items():
-				print(f"\033[4;33m{key}\033[0m :{val}")
+		for key, val in data[arg.css.rstrip()].items():
+			print(f"\033[4;33m{key}\033[0m :{val}")
 	except KeyError:
 		print("The CSS property that you requested doesn't exist/isn't indexed")
 
@@ -104,13 +104,13 @@ elif arg.css != "scc_empty":
 if not arg.js:
 	with open(f"{home}/.scc/ccs/en/js.json") as file:
 		data = json.load(file)
-		print("\n".join(data))
+	print("\n".join(data))
 elif arg.js != "scc_empty":
 	try:
 		with open(f"{home}/.scc/ccs/en/js.json") as file:
 			data = json.load(file)
-			for key, val in data[arg.js.rstrip()].items():
-				print(f"\033[4;33m{key}\033[0m : {val}")
+		for key, val in data[arg.js.rstrip()].items():
+			print(f"\033[4;33m{key}\033[0m : {val}")
 	except KeyError:
 		print(f"'{arg.js}' Not found")
 		try:
